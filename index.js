9c1e839ba069c0b9b16ffbd9a126d13517064f69
tulbureandreita <tulbureandreit@gmail.com>
2025-06-16 20:39:52 +0300

    server logic

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const folderRoutes = require("./routes/folderRoutes");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Expresss!");
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.use("/photos", express.static("D:/AQUAPARK_PHOTOS"));
+
+app.use("/api", folderRoutes);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
